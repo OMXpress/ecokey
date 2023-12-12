b59fb1314d184b469ad7a089b17f9f5fb33907e9
OMXpress <oma.mohamed@nu.edu.eg>
2023-12-12 21:00:06 +0200

    about us landing

--- Script/service.js
+++ Script/service.js
@@ -1,23 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var sections = document.querySelectorAll(".section");
+// document.addEventListener("DOMContentLoaded", function () {
+//     var sections = document.querySelectorAll(".section");
 
-    var options = {
-        root: null,
-        rootMargin: "0px",
-        threshold: 0.5,
-    };
+//     var options = {
+//         root: null,
+//         rootMargin: "0px",
+//         threshold: 0.5,
+//     };
 
-    var observer = new IntersectionObserver(function (entries, observer) {
-        entries.forEach(function (entry) {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-            }
-        });
-    }, options);
+//     var observer = new IntersectionObserver(function (entries, observer) {
+//         entries.forEach(function (entry) {
+//             if (entry.isIntersecting) {
+//                 entry.target.classList.add("visible");
+//             }
+//         });
+//     }, options);
 
-    sections.forEach(function (section) {
-        observer.observe(section);
-    });
-});
-const scrollBottom = document.querySelectorAll(".scroll-bottom");
-scrollBottom.forEach((el)=>observer.observe(el))
+//     sections.forEach(function (section) {
+//         observer.observe(section);
+//     });
+// });
+// const scrollBottom = document.querySelectorAll(".scroll-bottom");
+// scrollBottom.forEach((el)=>observer.observe(el))
